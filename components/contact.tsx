7a82a3d75af1cef47e4f6cacd901f6f19cccc833
DarkMorta1 <163432645+DarkMorta1@users.noreply.github.com>
2025-11-21 05:12:21 +0545

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Badge } from "@/components/ui/badge"
 import { Phone, Mail, MapPin, Facebook, Instagram, Linkedin, Send, CheckCircle, MessageSquare } from "lucide-react"
-import toast from "react-hot-toast"
+import { Toaster, toast } from "react-hot-toast";
 import { sendEmail } from "@/app/actions/send-email"
 
 export default function Contact() {
